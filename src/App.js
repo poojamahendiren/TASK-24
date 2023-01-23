5ee3e6c00b23653e889790bae42231bb5661fa7a
pooja <poojamahendiren99@gmail.com>
2023-01-23 13:52:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,277 @@
+import React, { useState} from 'react'
+import './App.css';
+import IMG1 from "./images/img1.webp";
+import IMG2 from "./images/img2.webp";
+import IMG3 from "./images/img3.webp";
+import IMG4 from "./images/img4.webp";
+import IMG5 from "./images/img5.webp";
+import IMG7 from "./images/img7.webp";
+import IMG8 from "./images/img8.webp";
+import IMG6 from "./images/img62.webp";
+function App() {
+    const[initialValue,setInitialValue]=useState(0);
+    const handleIncrement = (e) => {
+        if(e.target.innerText.trim("")==="Add to cart"){
+            setInitialValue(initialValue+1);
+            e.target.innerText="Remove from cart";
+        }
+        else{
+            setInitialValue(initialValue-1);
+            e.target.innerText="Add to cart";
+        }
+    }
+  return (
+    <body>
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">{initialValue}</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Elegant watches</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Let the Sun shine in your hand !!!</p>
+                </div>
+            </div>
+        </header>
+        
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                           
+                            <img class="card-img-top" src={IMG1} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 1</h5>
+                                    
+                                    $40.00 - $80.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                           
+                            <div class="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div>
+                            
+                            <img class="card-img-top" src={IMG2} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 2</h5>
+                                    
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    
+                                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <div class="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div>
+                            
+                            <img class="card-img-top" src={IMG3} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 3</h5>
+                                    
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $25.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <img class="card-img-top" src={IMG4} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 4</h5>
+                                    
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    
+                                    $40.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <div class="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div>
+                            
+                            <img class="card-img-top" src={IMG5} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 5</h5>
+                                    
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $25.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <img class="card-img-top" src={IMG6} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 6</h5>
+                                    
+                                    $120.00 - $280.00
+                                </div>
+                            </div>
+                            
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <div class="badge bg-dark text-white position-absolute" style={{top: '0.5rem', right: '0.5rem'}}>Sale</div>
+                           
+                            <img class="card-img-top" src={IMG7} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 7</h5>
+                                   
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    
+                                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+                           
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            
+                            <img class="card-img-top" src={IMG8} alt="..." />
+                            
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    
+                                    <h5 class="fw-bolder">Item 8</h5>
+                                    
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    
+                                    $40.00
+                                </div>
+                            </div>
+                           
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#" onClick={(e)=>handleIncrement(e)}>Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        
+        </body>
+
+   
+  );
+}
+
+export default App;
